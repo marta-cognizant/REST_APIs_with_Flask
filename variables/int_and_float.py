8a6d200b25dcecf5692a0656508a4fbf0e643cf8
Marta <marta.kolodzinska@cognizant.com>
2025-09-22 13:52:02 +0200

    [PY_REFRESHER] Variables

---
+++ variables/int_and_float.py
@@ -0,0 +1,15 @@
+# Create variables and assign values to them.
+# Example: 
+x = 15  # integer variable
+price = 9.99 # float variable
+discount = 0.2
+result = price * (1 - discount)
+print(result)
+
+
+# --------------
+a = 25
+b = a
+print(a, b)
+b = 17
+print(a, b)
---
+++ variables/string.py
@@ -0,0 +1,5 @@
+name = "Rolf" # Could be any also 'Rolf'
+# Overwrite the variable
+name = "Bob"
+print(name)  # Output: Bob
+print(name * 2)  # 
